8627fdf28f51cae775abe21374f12c36f20fe701
MelnichenkoOksana <melnicenkooksana1990@gmail.com>
2021-08-21 10:26:34 +0300

    Добавлен класс ввода-вывода

--- Task2/src/main/java/InputOutput.java
+++ Task2/src/main/java/InputOutput.java
@@ -1,2 +1,64 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class InputOutput {
+
+    public static String getInfoFromFile() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("INPUT.txt"));
+        StringBuffer sb = new StringBuffer();
+        sb.append(bufferedReader.readLine());
+        while(bufferedReader.ready()){
+            sb.append("\n");
+            sb.append(bufferedReader.readLine());
+        }
+        return sb.toString();
+    }
+
+    public static ArrayList<char[][]> conversionStringAnArray(String incomingString){
+        incomingString = incomingString.toLowerCase(Locale.ROOT).replaceAll("\\s+",""); // удаляем из строки все пробелы и невидимые строку
+        ArrayList<char[][]> labyrinthMap =new ArrayList<>();
+        char[] tempCharArray = incomingString.toCharArray(); //разбиваем строку на массив символов
+        int numberFloors = Character.getNumericValue(tempCharArray[0]);//кол-во этажей
+        int length = Character.getNumericValue(tempCharArray[1]); //длина этажа
+        int width = Character.getNumericValue(tempCharArray[2]); //ширина этажа
+
+        int counterSquare = 0;
+        int counterLength = 0;
+        int counterWidth = 0;
+        for (int i = 3; i < tempCharArray.length; i++) {
+            char [][] floor = new char[length][width];
+            while (counterSquare<((length*width))){
+                while (counterLength<length){
+                    while (counterWidth<width){
+                        floor[counterLength][counterWidth]=tempCharArray[i];
+                        counterWidth++;
+                        counterSquare++;
+                        i++;
+                    }
+                    counterLength++;
+                    counterWidth =0;
+                }
+                labyrinthMap.add(floor);
+                counterSquare = 0;
+            }
+        }
+
+        for (char[][] f:
+             labyrinthMap) {
+            System.out.println(f);
+        }
+        return labyrinthMap;
+    }
+
+    public static boolean writeFile(String filename, String resultText){
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(filename)))
+        {
+            bw.write(resultText);
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+        return true;
+    }
 }
--- Task2/src/main/java/Main.java
+++ Task2/src/main/java/Main.java
@@ -1,3 +1,33 @@
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        String s = "3 3 3\n" +
+                "\n" +
+                " \n" +
+                "\n" +
+                "1..\n" +
+                "\n" +
+                "oo.\n" +
+                "\n" +
+                "...\n" +
+                "\n" +
+                " \n" +
+                "\n" +
+                "ooo\n" +
+                "\n" +
+                "..o\n" +
+                "\n" +
+                ".oo\n" +
+                "\n" +
+                " \n" +
+                "\n" +
+                "ooo\n" +
+                "\n" +
+                "o..\n" +
+                "\n" +
+                "o.2";
+var result = InputOutput.conversionStringAnArray(InputOutput.getInfoFromFile());
+    }
 
 }
